c104c600c70685d5e3d571b067442df5f8411027
foolishmortalbuilders <63371547+foolishmortalbuilders@users.noreply.github.com>
2020-04-25 18:47:39 -0400

    Update install.sh
    Fix for pip3 install command

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt-get -y upgrade
-sudo apt-get install -y python3 python3-pip pygame
-sudo apt-get install -y PySDL2 libsdl1.2-dev libsdl-mixer1.2
+sudo apt-get install -ypython3 python3-pip 
+sudo apt-get install -y  libsdl1.2-dev libsdl-mixer1.2
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo apt-get install -y python-smbus
 sudo pip3 install --upgrade setuptools
 sudo pip3 install RPI.GPIO
-sudo pip3 install nfcp
+sudo pip3 install PySDL2 pygame nfcpy
 amixer set PCM 100%
