bf29fbdd1ec8ce708e44405f2fa925dd463c7624
mikeybg <v1mreed@yahoo.com>
2020-05-03 23:08:56 -0700

    missing space
    added space between -y and python3 on line 2

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y upgrade
-sudo apt-get install -ypython3 python3-pip 
-sudo apt-get install -y  libsdl1.2-dev libsdl-mixer1.2
+sudo apt-get install -y python3 python3-pip 
+sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo apt-get install -y python-smbus
 sudo pip3 install --upgrade setuptools
