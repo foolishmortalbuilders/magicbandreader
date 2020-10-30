df2b5ee3398abf8013e6cbff31c5a310eeba4c4e
scottoliver <scott.w.oliver@gmail.com>
2020-10-29 21:01:38 -0500

    Update install.sh
    sudo python3 magicband.py would not run without libsdl2-mixer-2.0-0 installed. 
    
    Error:
    Traceback (most recent call last):
      File "magicband.py", line 67, in <module>
        pygame.mixer.pre_init(44100, -16, 1, 512 )
      File "/usr/local/lib/python3.7/dist-packages/pygame/__init__.py", line 59, in __getattr__
        raise NotImplementedError(missing_msg)
    NotImplementedError: mixer module not available (ImportError: libSDL2_mixer-2.0.so.0: cannot open shared object file: No such file or directory)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y upgrade
 sudo apt-get install -y python3 python3-pip 
-sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2
+sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2 libsdl2-mixer-2.0-0
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo apt-get install -y python-smbus
 sudo pip3 install --upgrade setuptools
